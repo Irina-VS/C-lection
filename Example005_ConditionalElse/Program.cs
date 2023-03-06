c322ddc63f9fc56e843c4f9e707fa034d046eb04
Irina <grisha9419@yandex.ru>
2023-03-06 21:55:35 +0300

    add Example005 ConditionalElse

---
+++ Example005_ConditionalElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "пончик") 
+{
+    Console.WriteLine ("Ура, Пончик пришел!!!");
+}
+else
+{
+    Console.WriteLine ("Привет, ");
+    Console.WriteLine (username);
+}
